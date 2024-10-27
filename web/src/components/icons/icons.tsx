b59f1a81935bdab9c60965a5119d9234da557b03
Denis Shepelin <shepelin.denis@gmail.com>
2024-10-27 20:54:10 +0100

    Align the stroke width with other icons

--- web/src/components/icons/icons.tsx
+++ web/src/components/icons/icons.tsx
@@ -796,7 +796,7 @@ export const ReloadFeedback = ({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
+      stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
     >
